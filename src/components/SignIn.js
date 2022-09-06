6a1a48e3ff49db7cf72e68ae0762eefab9f831b2
ThiagoCCR <thiago.ccr96@gmail.com>
2022-09-06 14:47:07 -0300

    feat layout SignIn screen

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,119 @@
-
-
-
+import styled from "styled-components";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-  return <></>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignIn(e) {
+    return "oiii";
+  }
+
+  return (
+    <Wrapper>
+      <Container>
+        <h1>MyWallet</h1>
+        <Login>
+          <form onSubmit={handleSignIn}>
+            <input
+              name="email"
+              type="email"
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button type="submit">Entrar</button>
+          </form>
+        </Login>
+        <Link to={"/cadastro"}>
+          <SignUp>
+            <h2>Primeira vez? Cadastre-se!</h2>
+          </SignUp>
+        </Link>
+      </Container>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-family: Saira Stencil One;
+    font-size: 32px;
+    font-weight: 400;
+    line-height: 50px;
+    letter-spacing: 0em;
+    color: #ffffff;
+    margin-bottom: 5px;
+  }
+`;
+
+const Login = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    button {
+      width: 325px;
+      height: 60px;
+      font-family: "Raleway";
+      border-radius: 5px;
+      background-color: #a328d6;
+      font-weight: 700;
+      font-size: 20px;
+      border: none;
+      text-align: center;
+      color: #ffffff;
+    }
+  }
+  input {
+    font-weight: 400;
+    font-family: "Raleway";
+    border: none;
+    font-size: 20px;
+    width: 325px;
+    height: 60px;
+    border-radius: 5px;
+    color: #000000;
+    background-color: #ffffff;
+    margin-bottom: 10px;
+  }
+`;
+
+const SignUp = styled.div`
+  h2 {
+    margin-top: 10px;
+    text-decoration: underline;
+    font-family: "Raleway";
+    color: #ffffff;
+    font-size: 15px;
+    font-weight: 700;
+  }
+`;
